a701962f67f1285ae4621bfee867c5b27d5409f9
Thomas Dorsch <t.dorsch@ibgndt.de>
2025-08-13 07:13:54 +0000

    Replacing colorscheme with cattpuccin, adding custom init.lua
    Custom init.lua will overwrite / extend stuff which is placed in the
    main init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Set the blinking cursor settings
+vim.o.guicursor = 'n-v:block,i:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250'
+
+-- Make sure, that when we exit nvim the previous cursor settings get restored
+vim.cmd [[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:block20
+    augroup END
+]]
+
+-- Remap for dealing with word wrap in v mode
+vim.keymap.set('v', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('v', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 return {}
