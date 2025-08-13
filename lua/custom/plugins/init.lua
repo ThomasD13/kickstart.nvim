4be9d4536a6a3635ba542c77099eedc8301c9bb6
Thomas Dorsch <t.dorsch@ibgndt.de>
2025-08-13 07:29:27 +0000

    Adding smart-split and configure shortcuts in custom init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,4 +22,22 @@ vim.cmd [[
 vim.keymap.set('v', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('v', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Provide function and keymap binding to show not saved changes of current buffer
+function showBufferDiff()
+  vim.cmd [[w !diff % - ]]
+end
+-- ":vim.cmd([[w !diff % - ]])"
+vim.keymap.set('n', '<leader>pd', showBufferDiff, { expr = true, silent = true, desc = 'Print diff between buffer and file' })
+
+-- Mimic the tmux config here. S for vertical split, v for horizontal:
+-- - Remap for easier window control access
+-- - Remap general the window control access to <A-w>.
+-- - Remap (swapping) particular "s" to "v" when entering window control access.
+--   (Bear in mind that you have to release <A-w> keys, and then press "s" or "v".
+--   Otherwise the swapping of "s" and "v" does not work.
+local map = vim.api.nvim_set_keymap
+map('n', '<A-w>', '<C-w>', { noremap = false })
+map('n', '<C-w>s', '<C-w>v', { noremap = true })
+map('n', '<C-w>v', '<C-w>s', { noremap = true })
+
 return {}
